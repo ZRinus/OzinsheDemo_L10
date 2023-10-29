23bb12e1da394b5328b062fb54e9b322f47a6362
Rinat Zaripov <z_renat@inbox.ru>
2023-10-29 10:30:48 +0600

    Initial Commit

---
+++ OzinsheDemo_L10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OzinsheDemo_L10
+//
+//  Created by Rinat Zaripov on 29.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
